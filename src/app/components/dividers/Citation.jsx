28cde83cb76e26bd311c64ef03d282771576603b
Simon <simon.tessard@gmail.com>
2025-08-29 19:48:18 +0200

    Fix

--- src/app/components/dividers/Citation.jsx
+++ src/app/components/dividers/Citation.jsx
@@ -1,12 +1,13 @@
 import AnimText from "@/app/components/utils/AnimText";
+import CTABase from "@/app/components/ui/CTABase";
 
 export default function Citation() {
     return (
         <section
             id="separator"
-            className="bg-gradient-primary py-20 md:py-24 xl:py-36 2xl:py-50 overflow-hidden"
+            className="bg-gradient-primary py-20 md:py-24 xl:py-36 overflow-hidden"
         >
-            <div className="container">
+            <div className="container flex flex-col items-center gap-10 md:gap-14">
                 <AnimText
                     useScrollTrigger
                     tag="h3"
@@ -15,10 +16,18 @@ export default function Citation() {
                      leading-8 md:leading-10 xl:leading-13 2xl:leading-15"
                 >
                     « Je collabore seulement avec des entreprises
-                    qui veulent <span className="font-stix italic font-light"> créer </span> des expériences web
-                    <span className="font-stix italic font-light"> mémorables.</span> »
+                    qui veulent <span className="css-word-italic"> créer </span> des expériences web
+                    <span className="css-word-italic"> mémorables.</span> »
                 </AnimText>
+
+                <CTABase
+                    withScrollTrigger
+                    variant="white"
+                    to="/contact"
+                    text="Lancer mon projet"/>
             </div>
+
+
         </section>
     );
 }
