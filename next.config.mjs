40865976d5b4a26c9744ec5c5e6ac3ca82496aab
Simon <simon.tessard@gmail.com>
2025-08-31 14:37:00 +0200

    Optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    swcMinify: true,
+
     images: {
-        domains: ["images.unsplash.com"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+            },
+        ],
+        formats: ["image/avif", "image/webp"],
+        deviceSizes: [320, 640, 828, 1080, 1200, 1600, 1920],
     },
 };
 
