9a9ea2cf920c64df64d3c8735f88e24701c1fc77
Simon <simon.tessard@gmail.com>
2025-09-02 19:15:39 +0200

    Première personne

--- src/app/components/why/TwoColumns.jsx
+++ src/app/components/why/TwoColumns.jsx
@@ -1,11 +1,18 @@
 "use client";
 import pros from '@/data/pros.json';
-import {useEffect} from "react";
-import {gsap} from "gsap";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function TwoColumns() {
+    const itemsRef = useRef([]);
+
     useEffect(() => {
-        gsap.to('.js-two-columns--item', {
+        if (!itemsRef.current) return;
+
+        gsap.to(itemsRef.current, {
             y: 0,
             opacity: 1,
             stagger: 0.15,
@@ -13,36 +20,39 @@ export default function TwoColumns() {
             duration: 0.8,
             ease: "power2.out",
             scrollTrigger: {
-                trigger: '.js-two-columns--item',
+                trigger: itemsRef.current[0], // le premier élément comme déclencheur
                 start: 'top 80%',
                 end: 'bottom 20%',
-                toggleActions: 'play none none reverse',
             },
-        })
-    })
+        });
+    }, []);
+
     return (
         <div className="container grid grid-cols-1 gap-8 md:items-center">
-            <Column {...pros.freelance} />
+            <Column pros={pros.freelance.pros} itemsRef={itemsRef} />
         </div>
     );
 }
 
-function Column({ pros}) {
+function Column({ pros, itemsRef }) {
     return (
-            <ul className="grid grid-cols-1 md:grid-cols-2 w-full gap-4 md:gap-6 mb-4">
-                {pros.map((point, idx) => (
-                    <li className="js-two-columns--item translate-y-4 opacity-0 flex max-md:flex-col gap-3 md:gap-4 p-4 md:p-5 xl:p-6
-                    text-black bg-white w-full rounded-xl"
-                        key={idx}>
-                        <p className="w-full md:w-1/3 shrink-0 tracking-tight font-medium font-figtree
-                         text-lg xl:text-xl">
-                            {point.title}
-                        </p>
-                        <p className="w-full md:w-2/3 font-figtree tracking-tight font-light md:text-lg">
-                          {point.description}
-                        </p>
-                    </li>
-                ))}
-            </ul>
+        <ul className="grid grid-cols-1 md:grid-cols-2 w-full gap-4 md:gap-6 mb-4">
+            {pros.map((point, idx) => (
+                <li
+                    ref={el => itemsRef.current[idx] = el}
+                    key={idx}
+                    className="translate-y-4 opacity-0 flex max-md:flex-col gap-3 md:gap-4 p-4 md:p-5 xl:p-6
+                        text-black bg-white w-full rounded-xl"
+                >
+                    <p className="w-full md:w-1/3 shrink-0 tracking-tight font-medium font-figtree text-lg xl:text-xl">
+                        {point.title}
+                    </p>
+                    <p className="w-full md:w-2/3 font-figtree tracking-tight font-light md:text-lg">
+                        {point.description}
+                    </p>
+                </li>
+            ))}
+        </ul>
     );
 }
+
