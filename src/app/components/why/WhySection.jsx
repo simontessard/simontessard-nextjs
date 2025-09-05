8db6508bd8087fb75baabe68b624f4d675bf5ac4
Simon <simon.tessard@gmail.com>
2025-09-05 11:26:04 +0200

    Clean texte

--- src/app/components/why/WhySection.jsx
+++ src/app/components/why/WhySection.jsx
@@ -27,8 +27,8 @@ export default function WhySection() {
                 tag="p"
                 className="css-subtitle-section container text-white md:max-w-3xl mb-14 2xl:mb-24"
             >
-                Regardons ensemble les avantages concrets à
-                <br className="max-md:hidden"/> travailler avec moi plutôt qu'avec une agence.
+                Voici quelques avantages concrets à collaborer avec moi
+                <br className="max-md:hidden"/> plutôt qu’avec une agence.
             </AnimParagraph>
 
             <TwoColumns/>
