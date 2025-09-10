e10b6e593addf69e75c731d6cf0d2fa5613b3eea
Simon <simon.tessard@gmail.com>
2025-09-10 18:03:24 +0200

    Fonds centré

--- src/app/components/dividers/Citation.jsx
+++ src/app/components/dividers/Citation.jsx
@@ -1,12 +1,16 @@
 import AnimText from "@/app/components/utils/AnimText";
 import CTABase from "@/app/components/ui/CTABase";
+import Image from "next/image";
 
 export default function Citation() {
     return (
         <section
             id="separator"
-            className="bg-gradient-primary py-20 md:py-24 xl:py-36 overflow-hidden"
-        >
+            className="relative bg-gradient-primary py-20 md:py-24 xl:py-36 overflow-hidden">
+
+            <Image width={1000} height={1000} src="/logo.png" alt="Logo Simon TESSARD"
+                   className="absolute top-0 left-0 right-0 bottom-0 m-auto size-100 md:size-135 xl:size-175 opacity-2.5 object-contain"/>
+
             <div className="container flex flex-col items-center gap-10 md:gap-14">
                 <AnimText
                     useScrollTrigger
