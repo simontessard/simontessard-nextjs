1d5661f47d16d441f48bb5f3cbf383356ee16165
Simon <simon.tessard@gmail.com>
2025-09-15 17:53:28 +0200

    Fix

--- src/app/components/compare/Card.jsx
+++ src/app/components/compare/Card.jsx
@@ -25,7 +25,7 @@ export default function Card({ data }) {
     return (
         <div ref={cardRef} className={`opacity-0 flex flex-col py-5 px-6 md:px-7 bg-white rounded-xl border shadow-md w-full ${
             data.special ? "border-blue-primary/30 md:translate-x-12" : "border-gray-200 md:-translate-x-12" }`}>
-            <p className={`font-figtree font-medium tracking-tight text-center text-lg md:text-xl mb-3 md:mb-4 ${
+            <p className={`font-figtree font-medium tracking-tight text-center text-xl mb-3 md:mb-4 ${
                         data.special ? "css-text-gradient" : "" }`}>
                 {data.title}
             </p>
@@ -51,17 +51,17 @@ function InfoSection({ title, items, special }) {
             <p className={`text-sm font-figtree mb-1 ${special ? "text-blue-primary" : "text-gray-700" }`}>
                 {title}
             </p>
-            <ul className="flex flex-col gap-1 font-figtree font-medium">
+            <ul className="flex flex-col gap-1 font-figtree font-medium max-md:text-sm">
                 {items.map((item, idx) => (
                     <li key={idx} className="flex items-center gap-2">
                         {special ? (
                             // ✅ SVG Coche
-                            <svg className="shrink-0 size-3 fill-blue-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <svg className="shrink-0 size-2.5 md:size-3 fill-blue-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z" />
                             </svg>
                         ) : (
                             // ❌ SVG Croix
-                            <svg className="shrink-0 size-4 fill-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <svg className="shrink-0 size-3.5 md:size-4 fill-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <path d="M18.364 5.636l-1.414-1.414L12 9.172 7.05 4.222 5.636 5.636 10.586 10.586 5.636 15.536l1.414 1.414L12 12.828l4.95 4.95 1.414-1.414-4.95-4.95z" />
                             </svg>
                         )}
