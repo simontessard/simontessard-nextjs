2208f486498ad002eaaa0c0ab90559e69d5d8625
Simon <simon.tessard@gmail.com>
2025-09-24 17:06:21 +0200

    Sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://simontessard.fr',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: 'https://simontessard.fr/contact',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.5,
+        }
+    ]
+}
