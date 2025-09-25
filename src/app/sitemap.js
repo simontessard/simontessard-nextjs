c929e00925dbec4645fbe6a3da2c18b2d4fa2ceb
Simon <simon.tessard@gmail.com>
2025-09-25 22:29:29 +0200

    Sitemap update

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -13,10 +13,28 @@ export default function sitemap() {
             priority: 0.7,
         },
         {
-            url: 'https://simontessard.fr/contact',
+            url: 'https://simontessard.fr/offres',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.5,
+        },
+        {
+            url: 'https://simontessard.fr/process',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://simontessard.fr/contact',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.6,
+        },
+        {
+            url: 'https://simontessard.fr/legals',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.2,
         }
     ]
 }
