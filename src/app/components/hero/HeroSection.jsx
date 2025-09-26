b74688150671ec9903a5e16fac828975663b87a9
Simon <simon.tessard@gmail.com>
2025-09-26 18:28:20 +0200

    Nouveau Hero

--- src/app/components/hero/HeroSection.jsx
+++ src/app/components/hero/HeroSection.jsx
@@ -8,47 +8,50 @@ import HeroStar from "@/app/components/hero/HeroStar";
 
 export default function HeroSection() {
     return (
-        <section
-            id="hero"
-            className="relative bg-blue-primary pt-12 md:pt-24 min-h-[100vh]
+        <section id="hero" className="flex flex-col min-h-[100vh] bg-white max-h-[100vh] pb-4 px-4 md:pb-5 md:px-5">
+            <div className="relative block h-16 md:h-21 w-full">
+            </div>
+
+            <div className="relative bg-blue-200 grow size-full pt-12 md:pt-24 rounded-xl md:rounded-2xl
                  flex flex-col items-center justify-center overflow-hidden">
 
-            <HeroBackground />
+                <HeroBackground />
 
-            <div className="relative z-51 container flex justify-between items-end">
-                <div className="flex flex-col items-center mx-auto">
-                    <Uptitle className="text-white">
-                        Création de sites web
-                    </Uptitle>
+                <div className="relative z-51 container flex justify-between items-end">
+                    <div className="flex flex-col items-center mx-auto">
+                        <Uptitle className="text-white">
+                            Création de sites web
+                        </Uptitle>
 
-                    <AnimText
-                        tag="h1"
-                        className="text-white text-center font-figtree tracking-tighter text-5xl md:text-6xl xl:text-7xl 2xl:text-[5.5rem]
+                        <AnimText
+                            tag="h1"
+                            className="text-white text-center font-figtree tracking-tighter text-5xl md:text-6xl xl:text-7xl 2xl:text-[5.5rem]
                         leading-[1] md:leading-[1] xl:leading-[0.95] 2xl:leading-[0.9] font-medium mb-3 md:mb-5"
-                    >
-                        Une <span className="css-word-italic"> vitrine </span> web <br /> à la hauteur de <br /> votre{' '}
-                        <span className="css-word-italic relative">marque.
+                        >
+                            Une <span className="css-word-italic"> vitrine </span> web <br /> à la hauteur de <br /> votre{' '}
+                            <span className="css-word-italic relative">marque.
                            <HeroStar/>
                         </span>
-                    </AnimText>
+                        </AnimText>
 
-                    <AnimParagraph
-                        tag="p"
-                        className="font-figtree font-light text-center text-white/95 tracking-tight leading-[1.6]
+                        <AnimParagraph
+                            tag="p"
+                            className="font-figtree font-light text-center text-white/95 tracking-tight leading-[1.6]
                        lg:text-xl mb-6 md:mb-8">
-                        Freelance en création de sites web performants et design, <br className="max-md:hidden"/>
-                        conçus sur-mesure et réfléchis pour marquer les esprits.
-                    </AnimParagraph>
+                            Freelance en création de sites web performants et design, <br className="max-md:hidden"/>
+                            conçus sur-mesure et réfléchis pour marquer les esprits.
+                        </AnimParagraph>
 
-                    <div className="flex max-md:flex-col max-md:items-center size-fit gap-4 mb-8 md:mb-10">
-                        <CTABase
-                            variant="white"
-                            to="/contact"
-                            text="Réserver un appel"
-                        />
+                        <div className="flex max-md:flex-col max-md:items-center size-fit gap-4 mb-8 md:mb-10">
+                            <CTABase
+                                variant="white"
+                                to="/contact"
+                                text="Réserver un appel"
+                            />
+                        </div>
+
+                        <HeroSatisfaction/>
                     </div>
-
-                    <HeroSatisfaction/>
                 </div>
             </div>
         </section>
