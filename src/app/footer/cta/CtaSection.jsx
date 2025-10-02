b5d68f19d9c10247c854afd036d05fc6fb203fa0
Simon <simon.tessard@gmail.com>
2025-10-02 21:50:33 +0200

    Clean

--- src/app/footer/cta/CtaSection.jsx
+++ src/app/footer/cta/CtaSection.jsx
@@ -14,20 +14,18 @@ export default function CtaSection() {
     return (
         <section id="cta" className="p-3 md:p-5">
 
-            <div style={{backgroundImage: "url(/test.png)"}} className="bg-gradient-primary bg-center bg-cover rounded-xl">
+            <div style={{backgroundImage: "url(/cta.png)"}} className="bg-gradient-primary bg-center bg-cover rounded-xl">
                 <div className="container relative flex flex-col items-center justify-center shadow-sm overflow-hidden
-             py-12 md:py-16 px-6 md:px-8 xl:px-30 2xl:px-40 rounded-t-xl md:rounded-t-2xl text-center text-white size-full">
+                     py-12 md:py-16 px-6 md:px-8 xl:px-30 2xl:px-40 rounded-t-xl md:rounded-t-2xl text-center text-white size-full">
 
                     <Uptitle className="text-white" withScrollTrigger>
                         Prêt à démarrer ?
                     </Uptitle>
 
                     <AnimText
-                        useScrollTrigger
-                        tag="h2"
-                        className="font-figtree tracking-tight
-                    text-3xl md:text-4xl lg:text-6xl xl:text-7xl 2xl:text-7xl
-                    leading-[.95] font-medium mb-2 md:mb-4 xl:mb-6">
+                        useScrollTrigger tag="h2"
+                        className="font-figtree tracking-tight text-3xl md:text-4xl lg:text-6xl xl:text-7xl 2xl:text-7xl
+                        leading-[.95] font-medium mb-2 md:mb-4 xl:mb-6">
                         Lancez <span className="css-word-italic">votre</span> projet <br />
                         dès <span className="css-word-italic">aujourd&apos;hui.</span>
                     </AnimText>
